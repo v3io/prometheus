024dedd3b5e93e8aebc655a641df7cfd6e34611f
Eran Duchan <pavius@gmail.com>
2018-08-23 17:43:08 +0300

    revert Makefile v3io changes

--- storage/tsdb/tsdb.go
+++ storage/tsdb/tsdb.go
@@ -11,186 +11,101 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Build this module only if tests are being run
+// +build test
+
 package tsdb
 
 import (
 	"context"
-	"encoding/json"
-	"os"
-	"reflect"
 	"sync"
 	"time"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/common/model"
-	"github.com/prometheus/prometheus/storage"
-	"github.com/prometheus/tsdb"
+	"unsafe"
 
 	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/level"
 	"github.com/pkg/errors"
-	"github.com/v3io/v3io-tsdb/pkg/config"
-	"github.com/v3io/v3io-tsdb/promtsdb"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/prometheus/prometheus/storage"
+	"github.com/prometheus/tsdb"
+	tsdbLabels "github.com/prometheus/tsdb/labels"
 )
 
 // ErrNotReady is returned if the underlying storage is not ready yet.
 var ErrNotReady = errors.New("TSDB not ready")
 
-var openedLogger log.Logger
-
 // ReadyStorage implements the Storage interface while allowing to set the actual
 // storage at a later point in time.
 type ReadyStorage struct {
-	mtx             sync.RWMutex
-	v3ioPromAdapter storage.Storage
-	logger          log.Logger
-	closed          bool
+	mtx sync.RWMutex
+	a   *adapter
 }
 
 // Set the storage.
 func (s *ReadyStorage) Set(db *tsdb.DB, startTimeMargin int64) {
-	var err error
-
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
 
-	// save the logger that was passed to Open()
-	s.logger = openedLogger
-
-	// get the config path
-	configPath := s.getConfigPath()
-	s.logger.Log("msg", "Creating initial v3io adapter", "configPath", configPath)
-
-	// create the initial v3io adapter
-	s.v3ioPromAdapter, err = s.createV3ioPromAdapater(configPath)
-	if err != nil {
-		level.Warn(s.logger).Log("msg", "Failed to create v3io prometheus adapter", "err", err.Error())
-	}
-
-	// watch configuration file for changes
-	s.watchConfigForChanges(configPath)
+	s.a = &adapter{db: db, startTimeMargin: startTimeMargin}
 }
 
 // Get the storage.
 func (s *ReadyStorage) Get() *tsdb.DB {
+	if x := s.get(); x != nil {
+		return x.db
+	}
 	return nil
+}
+
+func (s *ReadyStorage) get() *adapter {
+	s.mtx.RLock()
+	x := s.a
+	s.mtx.RUnlock()
+	return x
 }
 
 // StartTime implements the Storage interface.
 func (s *ReadyStorage) StartTime() (int64, error) {
-	return 0, ErrNotReady
+	if x := s.get(); x != nil {
+		return x.StartTime()
+	}
+	return int64(model.Latest), ErrNotReady
 }
 
 // Querier implements the Storage interface.
 func (s *ReadyStorage) Querier(ctx context.Context, mint, maxt int64) (storage.Querier, error) {
-	s.logger.Log("msg", "Querier requested", "mint", mint, "maxt", maxt)
-
-	if !reflect.ValueOf(s.v3ioPromAdapter).IsNil() {
-		return s.v3ioPromAdapter.Querier(ctx, mint, maxt) //NEW
+	if x := s.get(); x != nil {
+		return x.Querier(ctx, mint, maxt)
 	}
-
 	return nil, ErrNotReady
 }
 
 // Appender implements the Storage interface.
 func (s *ReadyStorage) Appender() (storage.Appender, error) {
-	s.logger.Log("msg", "Appended requested")
-
-	if !reflect.ValueOf(s.v3ioPromAdapter).IsNil() {
-		return s.v3ioPromAdapter.Appender() //NEW
+	if x := s.get(); x != nil {
+		return x.Appender()
 	}
-
 	return nil, ErrNotReady
 }
 
 // Close implements the Storage interface.
 func (s *ReadyStorage) Close() error {
-	s.closed = true
+	if x := s.Get(); x != nil {
+		return x.Close()
+	}
 	return nil
-}
-
-func (s *ReadyStorage) getConfigPath() string {
-
-	// Initialize V3IO Adapter
-	configPath := os.Getenv("V3IO_FILE_PATH")
-	if configPath == "" {
-		configPath = "/etc/v3io/v3io-tsdb.yaml"
-	}
-
-	return configPath
-}
-
-func (s *ReadyStorage) watchConfigForChanges(configPath string) error {
-	s.logger.Log("msg", "Starting to watch configuration for changes", "configPath", configPath)
-
-	// get initial file mtime
-	lastConfigFileInfo, err := os.Stat(configPath)
-	if err != nil {
-		return err
-	}
-
-	// in the background, check for mtime changes and reload config
-	go func() error {
-
-		for !s.closed {
-
-			// wait a bit
-			time.Sleep(3 * time.Second)
-
-			// get current file mtime
-			currentConfigFileInfo, err := os.Stat(configPath)
-			if err != nil {
-				level.Warn(s.logger).Log("msg", "Failed to get config file info", "err", err.Error())
-				continue
-			}
-
-			// compare mtimes
-			if lastConfigFileInfo.ModTime() != currentConfigFileInfo.ModTime() {
-				lastConfigFileInfo = currentConfigFileInfo
-
-				s.logger.Log("msg", "Config file modification detected, recreating adapter", "configPath", configPath)
-
-				newV3ioPromAdapter, err := s.createV3ioPromAdapater(configPath)
-				if err != nil {
-					level.Warn(s.logger).Log("msg", "Failed to create new v3io adapter", "err", err.Error())
-					continue
-				}
-
-				// swap old pointer with new (atomic)
-				s.v3ioPromAdapter = newV3ioPromAdapter
-			}
-		}
-
-		s.logger.Log("msg", "Stopping config file change watch")
-
-		return nil
-	}()
-
-	return nil
-}
-
-func (s *ReadyStorage) createV3ioPromAdapater(configPath string) (*promtsdb.V3ioPromAdapter, error) {
-	loadedConfig, err := config.LoadConfig(configPath)
-	if err != nil {
-		return nil, err
-	}
-
-	// if the disabled flag is set, don't create an adapter
-	if loadedConfig.Disabled {
-		s.logger.Log("msg", "Adapter is disabled, not creating")
-		return nil, nil
-	}
-
-	if jsonLoadedConfig, err := json.Marshal(&loadedConfig); err == nil {
-		s.logger.Log("msg", "Creating v3io adapter", "config", string(jsonLoadedConfig))
-	}
-
-	return promtsdb.NewV3ioProm(loadedConfig, nil, nil)
 }
 
 // Adapter return an adapter as storage.Storage.
 func Adapter(db *tsdb.DB, startTimeMargin int64) storage.Storage {
-	return nil
+	return &adapter{db: db, startTimeMargin: startTimeMargin}
+}
+
+// adapter implements a storage.Storage around TSDB.
+type adapter struct {
+	db              *tsdb.DB
+	startTimeMargin int64
 }
 
 // Options of the DB storage.
@@ -214,9 +129,167 @@ type Options struct {
 
 // Open returns a new storage backed by a TSDB database that is configured for Prometheus.
 func Open(path string, l log.Logger, r prometheus.Registerer, opts *Options) (*tsdb.DB, error) {
+	if opts.MinBlockDuration > opts.MaxBlockDuration {
+		opts.MaxBlockDuration = opts.MinBlockDuration
+	}
+	// Start with smallest block duration and create exponential buckets until the exceed the
+	// configured maximum block duration.
+	rngs := tsdb.ExponentialBlockRanges(int64(time.Duration(opts.MinBlockDuration).Seconds()*1000), 10, 3)
 
-	// save the logger
-	openedLogger = l
+	for i, v := range rngs {
+		if v > int64(time.Duration(opts.MaxBlockDuration).Seconds()*1000) {
+			rngs = rngs[:i]
+			break
+		}
+	}
 
-	return nil, nil
+	db, err := tsdb.Open(path, l, r, &tsdb.Options{
+		WALFlushInterval:  10 * time.Second,
+		RetentionDuration: uint64(time.Duration(opts.Retention).Seconds() * 1000),
+		BlockRanges:       rngs,
+		NoLockfile:        opts.NoLockfile,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+// StartTime implements the Storage interface.
+func (a adapter) StartTime() (int64, error) {
+	var startTime int64
+
+	if len(a.db.Blocks()) > 0 {
+		startTime = a.db.Blocks()[0].Meta().MinTime
+	} else {
+		startTime = int64(time.Now().Unix() * 1000)
+	}
+
+	// Add a safety margin as it may take a few minutes for everything to spin up.
+	return startTime + a.startTimeMargin, nil
+}
+
+func (a adapter) Querier(_ context.Context, mint, maxt int64) (storage.Querier, error) {
+	q, err := a.db.Querier(mint, maxt)
+	if err != nil {
+		return nil, err
+	}
+	return querier{q: q}, nil
+}
+
+// Appender returns a new appender against the storage.
+func (a adapter) Appender() (storage.Appender, error) {
+	return appender{a: a.db.Appender()}, nil
+}
+
+// Close closes the storage and all its underlying resources.
+func (a adapter) Close() error {
+	return a.db.Close()
+}
+
+type querier struct {
+	q tsdb.Querier
+}
+
+func (q querier) Select(_ *storage.SelectParams, oms ...*labels.Matcher) (storage.SeriesSet, error) {
+	ms := make([]tsdbLabels.Matcher, 0, len(oms))
+
+	for _, om := range oms {
+		ms = append(ms, convertMatcher(om))
+	}
+	set, err := q.q.Select(ms...)
+	if err != nil {
+		return nil, err
+	}
+	return seriesSet{set: set}, nil
+}
+
+func (q querier) LabelValues(name string) ([]string, error) { return q.q.LabelValues(name) }
+func (q querier) Close() error                              { return q.q.Close() }
+
+type seriesSet struct {
+	set tsdb.SeriesSet
+}
+
+func (s seriesSet) Next() bool         { return s.set.Next() }
+func (s seriesSet) Err() error         { return s.set.Err() }
+func (s seriesSet) At() storage.Series { return series{s: s.set.At()} }
+
+type series struct {
+	s tsdb.Series
+}
+
+func (s series) Labels() labels.Labels            { return toLabels(s.s.Labels()) }
+func (s series) Iterator() storage.SeriesIterator { return storage.SeriesIterator(s.s.Iterator()) }
+
+type appender struct {
+	a tsdb.Appender
+}
+
+func (a appender) Add(lset labels.Labels, t int64, v float64) (uint64, error) {
+	ref, err := a.a.Add(toTSDBLabels(lset), t, v)
+
+	switch errors.Cause(err) {
+	case tsdb.ErrNotFound:
+		return 0, storage.ErrNotFound
+	case tsdb.ErrOutOfOrderSample:
+		return 0, storage.ErrOutOfOrderSample
+	case tsdb.ErrAmendSample:
+		return 0, storage.ErrDuplicateSampleForTimestamp
+	case tsdb.ErrOutOfBounds:
+		return 0, storage.ErrOutOfBounds
+	}
+	return ref, err
+}
+
+func (a appender) AddFast(_ labels.Labels, ref uint64, t int64, v float64) error {
+	err := a.a.AddFast(ref, t, v)
+
+	switch errors.Cause(err) {
+	case tsdb.ErrNotFound:
+		return storage.ErrNotFound
+	case tsdb.ErrOutOfOrderSample:
+		return storage.ErrOutOfOrderSample
+	case tsdb.ErrAmendSample:
+		return storage.ErrDuplicateSampleForTimestamp
+	case tsdb.ErrOutOfBounds:
+		return storage.ErrOutOfBounds
+	}
+	return err
+}
+
+func (a appender) Commit() error   { return a.a.Commit() }
+func (a appender) Rollback() error { return a.a.Rollback() }
+
+func convertMatcher(m *labels.Matcher) tsdbLabels.Matcher {
+	switch m.Type {
+	case labels.MatchEqual:
+		return tsdbLabels.NewEqualMatcher(m.Name, m.Value)
+
+	case labels.MatchNotEqual:
+		return tsdbLabels.Not(tsdbLabels.NewEqualMatcher(m.Name, m.Value))
+
+	case labels.MatchRegexp:
+		res, err := tsdbLabels.NewRegexpMatcher(m.Name, "^(?:"+m.Value+")$")
+		if err != nil {
+			panic(err)
+		}
+		return res
+
+	case labels.MatchNotRegexp:
+		res, err := tsdbLabels.NewRegexpMatcher(m.Name, "^(?:"+m.Value+")$")
+		if err != nil {
+			panic(err)
+		}
+		return tsdbLabels.Not(res)
+	}
+	panic("storage.convertMatcher: invalid matcher type")
+}
+
+func toTSDBLabels(l labels.Labels) tsdbLabels.Labels {
+	return *(*tsdbLabels.Labels)(unsafe.Pointer(&l))
+}
+
+func toLabels(l tsdbLabels.Labels) labels.Labels {
+	return *(*labels.Labels)(unsafe.Pointer(&l))
 }
---
+++ storage/tsdb/v3io.go
@@ -0,0 +1,225 @@
+// Copyright 2017 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Don't build this module when running tests
+// +build !test
+
+package tsdb
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"reflect"
+	"sync"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/storage"
+	"github.com/prometheus/tsdb"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/pkg/errors"
+	"github.com/v3io/v3io-tsdb/pkg/config"
+	"github.com/v3io/v3io-tsdb/promtsdb"
+)
+
+// ErrNotReady is returned if the underlying storage is not ready yet.
+var ErrNotReady = errors.New("TSDB not ready")
+
+var openedLogger log.Logger
+
+// ReadyStorage implements the Storage interface while allowing to set the actual
+// storage at a later point in time.
+type ReadyStorage struct {
+	mtx             sync.RWMutex
+	v3ioPromAdapter storage.Storage
+	logger          log.Logger
+	closed          bool
+}
+
+// Set the storage.
+func (s *ReadyStorage) Set(db *tsdb.DB, startTimeMargin int64) {
+	var err error
+
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+
+	// save the logger that was passed to Open()
+	s.logger = openedLogger
+
+	// get the config path
+	configPath := s.getConfigPath()
+	s.logger.Log("msg", "Creating initial v3io adapter", "configPath", configPath)
+
+	// create the initial v3io adapter
+	s.v3ioPromAdapter, err = s.createV3ioPromAdapater(configPath)
+	if err != nil {
+		level.Warn(s.logger).Log("msg", "Failed to create v3io prometheus adapter", "err", err.Error())
+	}
+
+	// watch configuration file for changes
+	s.watchConfigForChanges(configPath)
+}
+
+// Get the storage.
+func (s *ReadyStorage) Get() *tsdb.DB {
+	return nil
+}
+
+// StartTime implements the Storage interface.
+func (s *ReadyStorage) StartTime() (int64, error) {
+	return 0, ErrNotReady
+}
+
+// Querier implements the Storage interface.
+func (s *ReadyStorage) Querier(ctx context.Context, mint, maxt int64) (storage.Querier, error) {
+	s.logger.Log("msg", "Querier requested", "mint", mint, "maxt", maxt)
+
+	if !reflect.ValueOf(s.v3ioPromAdapter).IsNil() {
+		return s.v3ioPromAdapter.Querier(ctx, mint, maxt) //NEW
+	}
+
+	return nil, ErrNotReady
+}
+
+// Appender implements the Storage interface.
+func (s *ReadyStorage) Appender() (storage.Appender, error) {
+	s.logger.Log("msg", "Appended requested")
+
+	if !reflect.ValueOf(s.v3ioPromAdapter).IsNil() {
+		return s.v3ioPromAdapter.Appender() //NEW
+	}
+
+	return nil, ErrNotReady
+}
+
+// Close implements the Storage interface.
+func (s *ReadyStorage) Close() error {
+	s.closed = true
+	return nil
+}
+
+func (s *ReadyStorage) getConfigPath() string {
+
+	// Initialize V3IO Adapter
+	configPath := os.Getenv("V3IO_FILE_PATH")
+	if configPath == "" {
+		configPath = "/etc/v3io/v3io-tsdb.yaml"
+	}
+
+	return configPath
+}
+
+func (s *ReadyStorage) watchConfigForChanges(configPath string) error {
+	s.logger.Log("msg", "Starting to watch configuration for changes", "configPath", configPath)
+
+	// get initial file mtime
+	lastConfigFileInfo, err := os.Stat(configPath)
+	if err != nil {
+		return err
+	}
+
+	// in the background, check for mtime changes and reload config
+	go func() error {
+
+		for !s.closed {
+
+			// wait a bit
+			time.Sleep(3 * time.Second)
+
+			// get current file mtime
+			currentConfigFileInfo, err := os.Stat(configPath)
+			if err != nil {
+				level.Warn(s.logger).Log("msg", "Failed to get config file info", "err", err.Error())
+				continue
+			}
+
+			// compare mtimes
+			if lastConfigFileInfo.ModTime() != currentConfigFileInfo.ModTime() {
+				lastConfigFileInfo = currentConfigFileInfo
+
+				s.logger.Log("msg", "Config file modification detected, recreating adapter", "configPath", configPath)
+
+				newV3ioPromAdapter, err := s.createV3ioPromAdapater(configPath)
+				if err != nil {
+					level.Warn(s.logger).Log("msg", "Failed to create new v3io adapter", "err", err.Error())
+					continue
+				}
+
+				// swap old pointer with new (atomic)
+				s.v3ioPromAdapter = newV3ioPromAdapter
+			}
+		}
+
+		s.logger.Log("msg", "Stopping config file change watch")
+
+		return nil
+	}()
+
+	return nil
+}
+
+func (s *ReadyStorage) createV3ioPromAdapater(configPath string) (*promtsdb.V3ioPromAdapter, error) {
+	loadedConfig, err := config.LoadConfig(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// if the disabled flag is set, don't create an adapter
+	if loadedConfig.Disabled {
+		s.logger.Log("msg", "Adapter is disabled, not creating")
+		return nil, nil
+	}
+
+	if jsonLoadedConfig, err := json.Marshal(&loadedConfig); err == nil {
+		s.logger.Log("msg", "Creating v3io adapter", "config", string(jsonLoadedConfig))
+	}
+
+	return promtsdb.NewV3ioProm(loadedConfig, nil, nil)
+}
+
+// Adapter return an adapter as storage.Storage.
+func Adapter(db *tsdb.DB, startTimeMargin int64) storage.Storage {
+	return nil
+}
+
+// Options of the DB storage.
+type Options struct {
+	// The interval at which the write ahead log is flushed to disc.
+	WALFlushInterval time.Duration
+
+	// The timestamp range of head blocks after which they get persisted.
+	// It's the minimum duration of any persisted block.
+	MinBlockDuration model.Duration
+
+	// The maximum timestamp range of compacted blocks.
+	MaxBlockDuration model.Duration
+
+	// Duration for how long to retain data.
+	Retention model.Duration
+
+	// Disable creation and consideration of lockfile.
+	NoLockfile bool
+}
+
+// Open returns a new storage backed by a TSDB database that is configured for Prometheus.
+func Open(path string, l log.Logger, r prometheus.Registerer, opts *Options) (*tsdb.DB, error) {
+
+	// save the logger
+	openedLogger = l
+
+	return nil, nil
+}
